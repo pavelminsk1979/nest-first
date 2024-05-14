e68baad9efbc7817fc8d21b3e36b65e28bcc3c83
pavelminsk1979 <pavelminsk1979@mail.ru>
2024-05-14 22:28:21 +0300

    start

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,10 +2,21 @@ import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
+/* @Controller()является декоратором,
+ который применяется к классу AppController. Он указывает,
+ что этот класс является контроллером. Контроллеры в NestJS отвечают за
+  обработку HTTP-запросов и определение маршрутов*/
 export class AppController {
+  /* Здесь используется механизм внедрения зависимостей.
+    Когда экземпляр AppController создается, NestJS автоматически
+   внедряет экземпляр AppService*/
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get() /*@Get() является декоратором, который применяется
+   к методу getHello(). Он указывает, что этот метод будет
+   обрабатывать HTTP GET-запросы
+   @Get()   в скобках пусто поэтому URL будет таким  "/"
+    на фронтенд возвращается строка*/
   getHello(): string {
     return this.appService.getHello();
   }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
-@Injectable()
+@Injectable() /*ДЕКОРАТОР-Помечает класс как инъектируемый...
+Этот класс может быть инъектирован в другие компоненты или 
+классы (как зависимость) , это механизм внедрения зависимостей.*/
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World!"""';
   }
 }
